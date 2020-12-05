dbeafc29467a6be9e1adea5657789409176542e0
Manuel Zambrano <dev.mzambrano@gmail.com>
2020-12-06 00:14:07 +0100

    styles(blog): fix margin

--- layouts/index.js
+++ layouts/index.js
@@ -4,9 +4,9 @@ const Layout = ({ children, frontMatter }) => {
   return (
     <div className='blog'>
       <h1 className='mt-10 mb-4 title'>{frontMatter.title}</h1>
-      <div className='sm:flex justify-between grid grid-col-1'>
+      <div className='sm:flex justify-between grid grid-col-1 mb-10'>
         <span className='info'>🗓 {convertDate(frontMatter.publishedAt)} - ⏳ {getReadingTime(frontMatter.readingTime.time)}</span>
-        <small className='mb-10'>
+        <small>
           <a className='text-green-500 dark:text-green-500' href={frontMatter.github} target='_blank' rel='noreferrer'>¿Hay una errata? Edita este artículo.</a>
         </small>
       </div>
