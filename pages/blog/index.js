db0cb89c6f8221eb4e8231dbc1245ec1e6e7dfa6
Manuel Zambrano <dev.mzambrano@gmail.com>
2021-06-19 17:12:59 +0200

    fix posts order

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,7 +2,7 @@ import PostList from '../../components/PostList'
 
 const Blog = () => {
   return (
-    <div className="page-container">
+    <div className='page-container'>
       <PostList />
     </div>
   )
--- utils/timeUtils.js
+++ utils/timeUtils.js
@@ -1,9 +1,9 @@
-import { formatRelative, parse } from 'date-fns'
+import { formatRelative, isAfter, isEqual, parse } from 'date-fns'
 import { es } from 'date-fns/locale'
 
 export const convertDate = (value) => {
   let format = 'dd-MM-yyyy'
-  
+
   if (value.split(' ').length > 1) {
     format = 'dd-MM-yyyy HH:mm:ss'
   }
@@ -14,4 +14,19 @@ export const getReadingTime = (value) => {
   const minutes = Math.ceil(value / 60000)
   const text = minutes > 1 ? 'minutos de lectura' : 'minuto de lectura'
   return `${minutes} ${text}`
+}
+
+export const compareDates = (value1, value2) => {
+  const date1 = new Date(convertToUSFormat(value1))
+  const date2 = new Date(convertToUSFormat(value2))
+  return isEqual(date1, date2) ? 0 : isAfter(date2, date1) ? 1 : -1
+}
+
+const convertToUSFormat = (date) => {
+  const dateArr = date.split(' ')
+  let result = dateArr[0].split('-').reverse().join('-')
+  if (dateArr.length > 1) {
+    result += ` ${dateArr[1]}`
+  }
+  return result
 }
